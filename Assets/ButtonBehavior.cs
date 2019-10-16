be0c2f426b5664a61bd25e6fd560305ea521f490
Alex Mollard <alex.mollard@mail.com>
2019-10-16 16:03:32 +1100

    First Commit

---
+++ Assets/ButtonBehavior.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public string ButtonName;
+    public bool IsMainMenu = false;
+    public bool IsOption = false;
+    public bool HighLighted = false;
+    public bool Pressed = false;
+    public bool IsFunctionButton = false;
+
+    // Options
+    public bool IsMultiChoice = false;
+    public Color DefaultColor = Color.white;
+    public float ButtonTimer = -10.0f;
+    public string[] Choices = null;
+    public string CurrentChoice = "";
+    public TextMeshProUGUI ChoiceDisplay;
+    public bool OptionEnabled = false;
+    public Image Indicator = null;
+    public Vector3 DefaultIndicatorSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DefaultColor = GetComponent<Image>().color;
+
+        if (transform.GetChild(0).GetComponent<TextMeshProUGUI>() != null)
+        {
+            ButtonName = transform.GetChild(0).GetComponent<TextMeshProUGUI>().text;
+        }
+
+
+        if (ButtonName == "Resolution")
+        {
+            Choices = new string[3] {"1920 x 1080", "480 x 720", "999 x 111" };
+        }
+
+        if (IsMultiChoice)
+        {
+            CurrentChoice = Choices[0];
+        }
+
+        if (Indicator != null)
+            DefaultIndicatorSize = Indicator.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ButtonTimer > 0)
+            ButtonTimer -= Time.deltaTime * 5;
+
+        if (Pressed)
+        {
+            GetComponent<Image>().color = new Color(0.3f,0.3f,0.3f);
+            if (ButtonTimer < 0.1f)
+            {
+                Pressed = false;
+            }
+        }
+        else
+        {
+            GetComponent<Image>().color = DefaultColor;
+        }
+
+        if (Indicator != null)
+        {
+            if (OptionEnabled)
+            {
+                Indicator.transform.localPosition = new Vector3(2.5f, -2.5f, 0.0f);
+                Indicator.transform.localScale = new Vector3(DefaultIndicatorSize.x - 0.1f, DefaultIndicatorSize.y - 0.1f, DefaultIndicatorSize.z - 0.1f);
+            }
+            else
+            {
+                Indicator.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+                Indicator.transform.localScale = DefaultIndicatorSize;
+            }
+        }
+
+
+
+        // For the main menu buttons
+        if (IsMainMenu)
+        {
+            if (HighLighted && !Pressed)
+            {
+                if (transform.localPosition.x < -250)
+                {
+                    transform.position = new Vector3(transform.position.x + 10, transform.position.y, transform.position.z);
+                }
+            }
+            else if(!Pressed)
+            {
+                if (transform.localPosition.x > -380)
+                {
+                    transform.position = new Vector3(transform.position.x - 10, transform.position.y, transform.position.z);
+                }
+                else
+                {
+                    if (transform.localPosition.x < -380)
+                    {
+                        transform.localPosition = new Vector3(-380, transform.localPosition.y, transform.localPosition.z);
+                    }
+                }
+            }
+        }
+
+        // For Options
+        if (IsOption)
+        {
+            if (HighLighted && !Pressed)
+            {
+                GetComponent<Image>().color = Color.grey;
+            }
+
+            if (OptionEnabled && Indicator != null)
+            {
+                Indicator.color = Color.green;
+            }
+            else if (Indicator != null)
+            {
+                Indicator.color = Color.red;
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public bool IsDisabled;
+    public List<GameObject> button;
+    public int ChoiceIndex = 0;
+    int buttonTotal;
+    int buttonIndex;
+    public GameObject MainMenuCanvas;
+    public GameObject OptionCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int children = transform.childCount;
+        for (int i = 0; i < children; ++i)
+        {
+            if (transform.GetChild(i).GetComponent<ButtonBehavior>() != null)
+            {
+                button.Add(transform.GetChild(i).gameObject);
+            }
+        }
+
+        if (button.Count > 0)
+        {
+            button[0].GetComponent<ButtonBehavior>().HighLighted = true;
+        }
+
+        buttonIndex = 0;
+        buttonTotal = button.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If menu is enabled
+        if (!IsDisabled)
+        {
+            // Main Menu Only
+            if (button[buttonIndex].GetComponent<ButtonBehavior>().IsMainMenu)
+            {
+                // Menu Up
+                if (Input.GetKeyUp(KeyCode.DownArrow))
+                {
+                    button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                    buttonIndex++;
+                    if (buttonIndex > buttonTotal - 1)
+                    {
+                        buttonIndex = 0;
+                    }
+                    button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                }
+
+                // Menu Down
+                if (Input.GetKeyUp(KeyCode.UpArrow))
+                {
+                    button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                    buttonIndex--;
+                    if (buttonIndex < 0)
+                    {
+                        buttonIndex = buttonTotal - 1;
+                    }
+                    button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                }
+            }
+
+            // Indicate button was pressed
+            if (Input.GetKeyUp(KeyCode.Space) && !button[buttonIndex].GetComponent<ButtonBehavior>().IsMultiChoice && !button[buttonIndex].GetComponent<ButtonBehavior>().IsFunctionButton)
+            {
+                button[buttonIndex].GetComponent<ButtonBehavior>().Pressed = true;
+
+                if (button[buttonIndex].GetComponent<ButtonBehavior>().OptionEnabled)
+                    button[buttonIndex].GetComponent<ButtonBehavior>().OptionEnabled = false;
+                else
+                    button[buttonIndex].GetComponent<ButtonBehavior>().OptionEnabled = true;
+
+                button[buttonIndex].GetComponent<ButtonBehavior>().ButtonTimer = 1.0f;
+            }
+            
+            // If Button Has a function
+            else if (Input.GetKeyUp(KeyCode.Space) && !button[buttonIndex].GetComponent<ButtonBehavior>().IsMultiChoice)
+            {                                        
+                ButtonFunction(button[buttonIndex].GetComponent<ButtonBehavior>().ButtonName);
+            }
+
+
+            // Options Only
+            if (button[buttonIndex].GetComponent<ButtonBehavior>().IsOption)
+            {
+                // MultiChoice
+                if (button[buttonIndex].GetComponent<ButtonBehavior>().IsMultiChoice)
+                {
+                    if (Input.GetKeyUp(KeyCode.RightArrow))
+                    {
+                        ChoiceIndex++;
+                        if (ChoiceIndex > button[buttonIndex].GetComponent<ButtonBehavior>().Choices.Length - 1)
+                        {
+                            ChoiceIndex = 0;
+                        }
+                        button[buttonIndex].GetComponent<ButtonBehavior>().CurrentChoice = button[buttonIndex].GetComponent<ButtonBehavior>().Choices[ChoiceIndex];
+                        button[buttonIndex].GetComponent<ButtonBehavior>().ChoiceDisplay.text = button[buttonIndex].GetComponent<ButtonBehavior>().CurrentChoice;
+                    }
+
+                    else if (Input.GetKeyUp(KeyCode.LeftArrow))
+                    {
+                        ChoiceIndex--;
+                        if (ChoiceIndex < 0)
+                        {
+                            ChoiceIndex = button[buttonIndex].GetComponent<ButtonBehavior>().Choices.Length - 1;
+                        }
+                        button[buttonIndex].GetComponent<ButtonBehavior>().CurrentChoice = button[buttonIndex].GetComponent<ButtonBehavior>().Choices[ChoiceIndex];
+                        button[buttonIndex].GetComponent<ButtonBehavior>().ChoiceDisplay.text = button[buttonIndex].GetComponent<ButtonBehavior>().CurrentChoice;
+                    }
+
+                    else if (Input.GetKeyUp(KeyCode.DownArrow) && buttonIndex == 6)
+                    {
+                        button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                        buttonIndex++;
+                        button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                    }
+
+                    else if (Input.GetKeyUp(KeyCode.UpArrow) && buttonIndex == 6)
+                    {
+                        button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                        buttonIndex = 2;
+                        button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                    }
+                }
+                // Non MultiCoice
+                else
+                {
+                    if (Input.GetKeyUp(KeyCode.LeftArrow))
+                    {
+                        if (buttonIndex == 3 || buttonIndex == 4 || buttonIndex == 5)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex -= 3;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 8)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex--;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                    }
+
+                    else if (Input.GetKeyUp(KeyCode.RightArrow))
+                    {
+                        if (buttonIndex == 0 || buttonIndex == 1 || buttonIndex == 2)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex += 3;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 7)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex++;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                    }
+
+                    else if (Input.GetKeyUp(KeyCode.DownArrow))
+                    {
+                        if (buttonIndex == 2 || buttonIndex == 5)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex = 6;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 8)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex = 3;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 7)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex = 0;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 0 || buttonIndex == 1 || buttonIndex == 3 || buttonIndex == 4)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex++;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                    }
+
+                    else if (Input.GetKeyUp(KeyCode.UpArrow))
+                    {
+                        if (buttonIndex == 6)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex = 2;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 8 || buttonIndex == 7)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex = 6;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 0)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex = 7;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 3)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex = 8;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                        else if (buttonIndex == 1 || buttonIndex == 2 || buttonIndex == 4 || buttonIndex == 5)
+                        {
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+                            buttonIndex--;
+                            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    void ButtonFunction(string buttonName)
+    {
+        // Main Menu
+        if (buttonName == "Play")
+        {
+            Debug.Log("Disable me and move to player selection");
+        }
+
+        if (buttonName == "Options")
+        {
+            OptionCanvas.SetActive(true);
+            IsDisabled = true;
+        }
+
+        if (buttonName == "Exit")
+        {
+            Application.Quit();
+        }
+
+        // Options
+        if (buttonName == "Close")
+        {
+            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = false;
+            buttonIndex = 0;
+            button[buttonIndex].GetComponent<ButtonBehavior>().HighLighted = true;
+            MainMenuCanvas.GetComponentInChildren<ButtonManager>().IsDisabled = false;
+            transform.parent.gameObject.SetActive(false);
+        }
+
+    }
+}
